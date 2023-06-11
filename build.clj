c0296082d5f1a9ce1062be21d18ee8aa27f037dd
Albert Snow <ajsnow2012@gmail.com>
2023-06-11 17:47:02 -0500

    frantically trying to get a prod uberjar working

--- build.clj
+++ build.clj
@@ -14,9 +14,11 @@
       (clean nil)
       (b/copy-dir {:src-dirs ["src" "resources"]
                    :target-dir class-dir})
+
       (b/compile-clj {:basis basis
                       :src-dirs ["src"]
                       :class-dir class-dir})
+
       (b/uber {:class-dir class-dir
                :uber-file uber-file
                :basis basis
